66f6145a74b8728972905dcbfc70b058c25443e2
GideonBrasil <gidxx_z@yahoo.com>
2019-02-12 22:55:37 +0000

    Add express_server.js to tinyapp project

---
+++ express_server.js
@@ -0,0 +1,16 @@
+let express = require('express');
+let app = express();
+let PORT = 8080;
+
+let urlDatabase = {
+    'b2xVn2': 'http://www.lighthouselabs.ca',
+    '9sm5xK': 'http://www.google.com'
+};
+
+app.get('/', (req, res) => {
+    res.send('Hello');
+});
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}!`);
+});
