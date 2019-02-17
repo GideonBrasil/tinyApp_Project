963bb2ff662c3fa16307944bde558cdaeacec7fa
GideonBrasil <gidxx_z@yahoo.com>
2019-02-17 23:58:35 +0000

    fixed users can only see their own TinyURL, users can only edit/delete own URLs, anyone can visit URLs

--- express_server.js
+++ express_server.js
@@ -2,16 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-app.get('/login', (req, res) => {
-    let templateVars = {
-        username: users[req.cookies['user_id']],
-    };
-    res.render('urls_login', templateVars);
-});
-
+//Functions used on server
 function generateRandomString() {
     //generates a string of 6 random alphanumeric characters
     let holder = '';
@@ -23,13 +19,22 @@ function generateRandomString() {
     return holder;    
 }
 
-// write function checkEmptyString and checkEmail   
+function urlsForUser(id){
+    let userURL = [];
+    for (let shortURL in urlDatabase){
+        if (urlDatabase[shortURL]['userID'] === id) {
+            userURL.push({
+                id,
+                shortURL,
+                link: urlDatabase[shortURL]['longURL']
+            });
+        }
+    }
+    return userURL;
+}
 
-// parses the input URL so that we can visualize it in req.body
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
-
-const users = {
+//Server Databases
+let users = {
     'userID': {
         id: 'userID',
         email: 'userEmail',
@@ -42,25 +47,20 @@ const users = {
     }
 };
 
-// object to store TinyURLs and full URLs 
-var urlDatabase = {
+let urlDatabase = {
     'b2xVn2': { 
         longURL: 'http://www.lighthouselabs.ca',
-        id: 'userID'
+        userID: 'userID'
     },
     '9sm5xK': { 
         longURL: 'http://www.google.com', 
-        id: 'userID2'
+        userID: 'userID2'
     }
 };
 
-
+//Server routes
 app.get('/', (req, res) => {
     res.send('I love dis');
-});
-
-app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}!`);
 });
 
 app.get('/urls.json', (req, res) => {
@@ -71,35 +71,19 @@ app.get('/hello', (req, res) => {
     res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
-app.get('/urls', (req, res) => {
-    let templateVars = { 
-        URLs: urlDatabase,
-        username: users[req.cookies['user_id']],
-    };
-    res.render('urls_index', templateVars);
-});
-
 app.get('/register', (req, res) => {
-    let templateVars = { URLs: urlDatabase, 
-        username: users[req.cookies['user_id']]};
+    let templateVars = { 
+        URLs: urlDatabase, 
+        username: req.cookies['user_id']
+    };
     res.render('urls_register', templateVars);
-});
-
-app.post('/urls/:shortURL/delete', (req, res) => {
-    delete  urlDatabase[req.params.shortURL];
-    res.redirect('/urls');
-});
-
-app.post('/logout', (req, res) => {
-    res.clearCookie('user_id');
-    res.redirect('/urls');
 });
 
 app.post('/register', (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
     const id = generateRandomString();
-    if (email === '' || password === '') {
+    if (!email || !password) {
         res.sendStatus(400);
     }
     for (let user in users) {
@@ -107,29 +91,27 @@ app.post('/register', (req, res) => {
             res.sendStatus(400);
         }
     }
-
     let userData = {
         id,
         email,
         password,
     };
     users[id] = userData;
-
+    console.log('userData:', userData);
+    console.log('urlDatabase:', urlDatabase);
+    console.log('users; ', users);
+    // console.log('urlDatabase: ', urlDatabase);
+    // console.log('users: ', users);
     res.cookie('user_id', id);
     res.redirect('/urls');
 });
 
-app.get('/urls/new', (req, res) => {
+app.get('/login', (req, res) => {
+    console.log('username:', users);
     let templateVars = {
-        username: users[req.cookies['user_id']],
-        id: users[req.cookies['user_id']]
+        username: req.cookies['user_id'],
     };
-    if (users[req.cookies['user_id']]) {
-        res.render('urls_new', templateVars);
-    } else {
-        res.redirect('/login');
-    }
-    
+    res.render('urls_login', templateVars);
 });
 
 app.post('/login', (req, res) => {
@@ -138,22 +120,33 @@ app.post('/login', (req, res) => {
     // res.cookie('username', req.body.username);
     // console.log('Cookie username:', req.body.username);
     const email = req.body.email;
+    console.log('email:', email);
     const password = req.body.password;
+    console.log('password:', password);
     for (let storedUser in users) {
-        if (users[storedUser].email === email) {
+        if ((users[storedUser].email === email) && (users[storedUser].password === password)) {
             res.cookie('user_id', storedUser);
-    
-        }
-        if (email === '' || password === '') {
-            res.sendStatus(400);
-        }
-        for (let user in users) {
-            if (email === users[user].email) {
-                res.sendStatus(400);
-            }
-        }
-        res.redirect('urls');
+            res.redirect('/urls');
+        }       
     }
+    res.sendStatus(400);
+});
+
+app.post('/logout', (req, res) => {
+    res.clearCookie('user_id');
+    res.redirect('/urls');
+});
+
+app.get('/urls', (req, res) => {
+    console.log(req.cookies['user_id']);
+    let templateVars = { 
+        URLs: urlDatabase,
+        username: users[req.cookies['user_id']],
+        userID: req.cookies['user_id']
+    };
+    console.log('urls GET -:', urlDatabase);
+    console.log('urls:users GET: ', users)
+    res.render('urls_index', templateVars);
 });
 
 app.post('/urls', (req, res) => {
@@ -161,26 +154,69 @@ app.post('/urls', (req, res) => {
     // put the random string function into a variable
     let randomURL = generateRandomString();
     // pass TinyURL into database as an object and request body of longURL
+    console.log('create new url, user id: ', req.cookies['user_id']);
     urlDatabase[randomURL] = {
         longURL: req.body.longURL,
-        userID: users[req.cookies['user_id']]
+        userID: req.cookies['user_id']
     };
     // redirect to urls_index
     res.redirect(`/urls/${randomURL}`);
     // console.log(urlDatabase);
 });
 
-app.get('/urls/:shortURL', (req, res) => {
-    let templateVars = { 
-        shortURL: req.params.shortURL, 
-        longURL: urlDatabase[req.params.shortURL].longURL,
+app.get('/urls/new', (req, res) => {
+    let templateVars = {
         username: users[req.cookies['user_id']]
     };
-    res.render('urls_show', templateVars);
+    if (users[req.cookies['user_id']]) {
+        res.render('urls_new', templateVars);
+    } else {
+        res.redirect('/login');
+    }
+});
+
+app.get('/urls/:shortURL', (req, res) => {
+    if (req.cookies['user_id'] === urlDatabase[req.params.shortURL].userID) {
+        let templateVars = { 
+            shortURL: req.params.shortURL, 
+            longURL: urlDatabase[req.params.shortURL].longURL,
+            username: users[req.cookies['user_id']]
+        };
+        res.render('urls_show', templateVars);
+    } else {
+        res.redirect(urlDatabase[req.params.shortURL].longURL);
+    }
+    
+});
+
+app.post('/urls/:id/delete', (req, res) => {
+    let randomShortURL = req.params.id;
+    console.log('POST delete: ', randomShortURL, urlDatabase, urlDatabase[randomShortURL]);
+    if (urlDatabase[randomShortURL].userID === req.cookies['user_id']) {
+        delete urlDatabase[randomShortURL];
+        res.redirect('/urls');
+    } else {
+        res.send('You don\'t have the permission to delete TinyURLs');
+    }
 });
 
 // post request for the edit form
 app.post('/urls/:id/edit', (req, res) => {
-    urlDatabase[req.params.id] = req.body.longURL;
-    res.redirect('/urls');
+    let shortURL = req.params.id;
+    // console.log('urlDatabase: ', urlDatabase);
+    // console.log(shortURL);
+    // console.log('cookies: ', req.cookies['user_id']);
+    let longURL = req.body.longURL;
+    // urlDatabase[shortURL] = {longURL: longURL, userID: req.cookies['user_id']};
+    console.log('compare:', req.cookies['user_id'], urlDatabase, shortURL, urlDatabase[shortURL]);
+    if (urlDatabase[shortURL].userID === req.cookies['user_id']) {
+        urlDatabase[shortURL]['longURL'] = longURL;
+        res.redirect('/urls');
+    } else {
+        res.send('You must log in to be able to edit this link');
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}!`);
 });
